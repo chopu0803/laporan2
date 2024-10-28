10944240bc0ea048044698e2654c5388c0d0cb11
GilangAdiss30 <adissfreedom672@gmail.com>
2024-10-28 08:36:59 +0700

    first commit

---
+++ lib/api.dart
@@ -0,0 +1,6 @@
+class Baseurl {
+  static String tambah = "http://192.168.100.128/biodata/insert.php";
+  static String data = "http://192.168.100.128/biodata/list.php";
+  static String edit = "http://192.168.100.128/biodata/update.php";
+  static String hapus = "http://192.168.100.128/biodata/delete.php";
+}
